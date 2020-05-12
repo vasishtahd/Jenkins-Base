43b62dbd815e52dee6492cf81edf711a23e86a1a
vasishtahd <30791332+vasishtahd@users.noreply.github.com>
2020-05-12 15:57:30 -0700

    Update JavaCheck

--- JavaCheck.java
+++ JavaCheck.java
@@ -3,7 +3,8 @@ class JavaCheck
 { 
   
     public static void main(String args[]) 
-    { 
+    {    
+         System.out.println( "Java Runtime Version"));
          System.out.println(System.getProperty("java.runtime.version"));
     } 
 } 
